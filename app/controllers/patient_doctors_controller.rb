60dd719cf0571596c65a8de9006ad902362bd59a
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-04-24 11:03:04 -0600

    feat: routes, update view, add destroy method

--- app/controllers/patient_doctors_controller.rb
+++ app/controllers/patient_doctors_controller.rb
@@ -1,2 +1,9 @@
 class PatientDoctorsController < ApplicationController
+  def destroy
+    @patient = Patient.find(params[:id])
+    @doctor = Doctor.find(params[:id])
+    @pateint_doctor = PatientDoctor.find_by(doctor_id: @doctor.id, patient_id: @patient.id)
+    @pateint_doctor.destroy
+    redirect_to doctor_path(@doctor)
+  end
 end
