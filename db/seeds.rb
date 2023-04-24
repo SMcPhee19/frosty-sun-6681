443a272b52a7f23cd35df00c9253642cb4792ecf
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-04-24 09:40:20 -0600

    feat: database setup and relationship testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Hospital.destroy_all
+Doctor.destroy_all
+Patient.destroy_all
+PatientDoctor.destroy_all
+
+@hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+@hospital2 = Hospital.create!(name: "Seaside Health & Wellness Center")
+
+@doctor1 = @hospital1.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+@doctor2 = @hospital1.doctors.create!(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")
+@doctor3 = @hospital2.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+@doctor4 = @hospital2.doctors.create!(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", university: "University of Pennsylvania")
+
+@patient1 = Patient.create!(name: "Katie Bryce", age: 24)
+@patient2 = Patient.create!(name: "Denny Duquette", age: 39)
+@patient3 = Patient.create!(name: "Rebecca Pope", age: 32)
+@patient4 = Patient.create!(name: "Zola Shepherd", age: 2)
