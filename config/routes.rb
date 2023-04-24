009e8e8dd8ec3bf13a7c35c9f5f1a81764e64f2e
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-04-24 10:03:11 -0600

    feat: add view, show method for doctors. complete: checklist for readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :patient_doctors
   resources :patients
+  resources :doctors, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
